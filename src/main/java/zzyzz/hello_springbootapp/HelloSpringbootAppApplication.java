e1d34dfc36395f666440eaf8aa422507c7d75d51
cOwO7 <oclc00@naver.com>
2025-01-06 10:58:09 +0900

    Initial commit

---
+++ src/main/java/zzyzz/hello_springbootapp/HelloSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package zzyzz.hello_springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringbootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringbootAppApplication.class, args);
+    }
+
+}
